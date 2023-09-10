12323743d2371a2344f1e97de42dfe04e1859df3
Khaled Naes <86200305+Khaled6120@users.noreply.github.com>
2023-09-10 23:57:36 +0300

    Create bankFactory.ts
    An example of Factory Design pattern using Typescript

---
+++ bankFactory.ts
@@ -0,0 +1,62 @@
+class BankFactory {
+  createBank(type: string, amount: number, sender: string): Bank {
+    switch (type) {
+      case 'bank_A':
+        return new Bank_A(amount, sender);
+      case 'bank_B':
+        return new Bank_B(amount, sender);
+      default:
+        return new Bank_Default(amount, sender); // Handle unknown bank types here
+    }
+  }
+}
+
+class Bank {
+  private amount: number;
+  private sender: string;
+
+  constructor(amount: number, sender: string) {
+    this.amount = amount;
+    this.sender = sender;
+  }
+
+  withdraw() {
+    // Default implementation for withdrawal
+    console.log('Default withdrawal');
+  }
+}
+
+class Bank_Default extends Bank {
+  constructor(amount: number, sender: string) {
+    super(amount, sender);
+  }
+
+  withdraw() {
+    console.log('Withdrawed by Default');
+  }
+}
+
+class Bank_A extends Bank {
+  constructor(amount: number, sender: string) {
+    super(amount, sender);
+  }
+
+  withdraw() {
+    console.log('Withdrawed by A');
+  }
+}
+
+class Bank_B extends Bank {
+  constructor(amount: number, sender: string) {
+    super(amount, sender);
+  }
+
+  withdraw() {
+    console.log('Withdrawed by B');
+  }
+}
+
+const bankFactory = new BankFactory();
+
+const a_Bank = bankFactory.createBank('bankd_B', 33433, 'df');
+a_Bank.withdraw();
